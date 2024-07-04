9ee9d4f33ce03ccd513297b645ac8ddab0af7606
yarz0 <40396015+yarz0@users.noreply.github.com>
2024-07-04 20:13:58 +1000

    Update module_3_4.py

--- module_3_4.py
+++ module_3_4.py
@@ -1,25 +1,12 @@
-# def single_root_words(root_words, *other_words):
-#     same_words = []
-#     for word in other_words:
-#         word.lower()
-#         root_words.lower()
-#         if word.count(word):
-#             same_words.append(word)
-#
-#         return same_words
-#
-#
-# print(single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies'))
-#
-# print(single_root_words('Disablement', 'able', 'mable', 'disable', 'bagel'))
+def single_root_words(root_words, *other_words):
+    same_words = []
+    for word in other_words:
+        word.lower()
+        root_words.lower()
+        if word.count(root_words[:4]) or  word.count(root_words[0:4]) or root_words.count(word[3:7]):
+            same_words.append(word)
 
-slovo = ['richiest', 'orichalcum', 'cheers', 'richies']
-root = 'Rich'
-result = []
-for word in slovo:
-    if word.lower().startswith(root.lower()) in slovo:
-        result.append(word)
-
-print(result)
-
-
+    return same_words 
+        
+print(single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies'))
+print(single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel'))
