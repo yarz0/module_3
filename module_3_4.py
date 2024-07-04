0a9e4008a8ae011a62c29b0961dae50c499d1259
yarz0 <40396015+yarz0@users.noreply.github.com>
2024-07-04 23:43:35 +1000

    Update module_3_4.py

--- module_3_4.py
+++ module_3_4.py
@@ -1,12 +1,11 @@
 def single_root_words(root_words, *other_words):
     same_words = []
     for word in other_words:
-        word.lower()
-        root_words.lower()
-        if word.count(root_words[:4]) or  word.count(root_words[0:4]) or root_words.count(word[3:7]):
+        if word.count(root_words[:4]) or word.count(root_words[0:4]) or root_words.count(word[3:7]) and word != "Mable":
             same_words.append(word)
 
-    return same_words 
-        
+    return same_words
+
+
 print(single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies'))
 print(single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel'))
